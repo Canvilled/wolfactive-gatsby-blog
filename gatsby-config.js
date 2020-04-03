6f6980ac5730f2edc5ccb07c0f6299a153345e23
huyminh nguyenpham <huyminhnguyenpham@gmail.com>
2020-04-03 13:02:29 +0700

    upload code PWA v2

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,17 +38,6 @@ module.exports = {
         hostingWPCOM: false,
       }
     },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Wolfactive Blogs Site`,
-        short_name: `WolfactiveBlogs`,
-        start_url: `/`,
-        background_color: `#f2f4fb`,
-        theme_color: `#1c6182`,
-        display: `standalone`,
-      },
-    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
