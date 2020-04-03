8287e46e33c9505b8c1d040955c6f3f4ae4b016f
huyminh nguyenpham <huyminhnguyenpham@gmail.com>
2020-04-03 13:07:57 +0700

    upload code social link

--- src/templates/post.js
+++ src/templates/post.js
@@ -28,16 +28,16 @@ export default ({pageContext}) =>(
             Theo dõi chúng tôi
           </h3>
           <div className="sidebar__cotent social--sidebar">
-            <a href="#" target="_blank" className="social--icon social--facebook">
+            <a href="https://www.facebook.com/Wolfactiveweb.design.SEO/" target="_blank" className="social--icon social--facebook">
               <i className="fab fa-facebook-square"></i>
             </a>
             <a href="#" target="_blank" className="social--icon social--instagram">
               <i className="fab fa-instagram"></i>
             </a>
-            <a href="#" target="_blank" className="social--icon social--behance">
+            <a href="https://www.behance.net/kieungan16c6e9" target="_blank" className="social--icon social--behance">
               <i className="fab fa-behance-square"></i>
             </a>
-            <a href="#" target="_blank" className="social--icon social--pinterest">
+            <a href="https://www.pinterest.com/kieungan16109/" target="_blank" className="social--icon social--pinterest">
               <i className="fab fa-pinterest-square"></i>
             </a>
           </div>
