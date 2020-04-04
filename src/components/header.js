7ee4ad2dd463e529cfd23b91c5a4cbe6a796d5e3
huyminh nguyenpham <huyminhnguyenpham@gmail.com>
2020-04-04 12:08:05 +0700

    upload code add more h1

--- src/components/header.js
+++ src/components/header.js
@@ -3,11 +3,13 @@ import logo from '../images/logo_nobg.png';
 import Navbar from './navbar.js';
 
 const Header = () => (
-  <header className="main--background">     
+  <header className="main--background">
     <div className="header__contain container">
       <div className="header__item">
           <div className="header__brand">
-            <a href="http://wolfactive.net/"><img data-src={logo} alt="Logo-wolfactive" className="logo lazyload" /></a>
+            <h1>
+              <a href="http://wolfactive.net/"><img data-src={logo} alt="Logo-wolfactive" className="logo lazyload" /></a>
+            </h1>
           </div>
       </div>
       <div className="header__item">
