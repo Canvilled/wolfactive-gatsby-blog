6a03ff4204e593953c8f83a613a50a63b1dd920a
huyminh nguyenpham <huyminhnguyenpham@gmail.com>
2020-04-04 11:32:40 +0700

    upload code GTM v2

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,11 @@ import logo from '../images/logo_nobg.png';
 import Navbar from './navbar.js';
 
 const Header = () => (
-  <header className="main--background">      
+  <header className="main--background">
+        <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TZ9DRTR"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     <div className="header__contain container">
       <div className="header__item">
           <div className="header__brand">
